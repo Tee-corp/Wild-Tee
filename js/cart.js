8a30e6d215a0870597f66edc2f75031e3dc0cd9c
monaSalih <monaalobisat@gmail.com>
2021-07-11 18:13:00 +0300

    fix close  button

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let  table = document.getElementById('cart');
+let table = document.getElementById('cart');
 let tbodyEl = document.getElementById('body');
 table.appendChild(tbodyEl);
 
 let tshirt = {
-    design : [],
-}
+  design : [],
+};
 
 function loadCart() {
-  let  tShirtItems = JSON.parse(localStorage.getItem('t-shirtInCart')) || [];
+  let tShirtItems = JSON.parse(localStorage.getItem('t-shirtInCart')) || [];
   tshirt.design= tShirtItems;
   console.log(tshirt);
 }
@@ -23,82 +23,83 @@ function renderCart() {
 
 function clearCart() {
 
-tbodyEl.textContent='';
+  tbodyEl.textContent='';
 }
 
 
 let trEl;
-  let tdEl;
-  let aEl;
+let tdEl;
+let aEl;
 function showCart() {
-  
-for (let i = 0; i < tshirt.design.length; i++) {
-   trEl=document.createElement('tr');
-  tdEl = document.createElement('td');
-   aEl =document.createElement('a');
-  aEl.setAttribute('id',`${i}`)
-  aEl.innerHTML='<i class="far fa-trash-alt"></i>';
-  aEl.addEventListener('click', removeItemFromCart);
-  tdEl.appendChild(aEl);
-  trEl.appendChild(tdEl);
-  let tdEl1 = document.createElement('td');
-  tdEl1.textContent=`${tshirt.design[i].quantity}`;
-  trEl.appendChild(tdEl1);
-  let tdEl2 = document.createElement('td');
-  tdEl2.textContent=`${tshirt.design[i].icon.split('.')[0]}`;
-  trEl.appendChild(tdEl2);
-  let tdEl3 = document.createElement('td');
-  tdEl3.textContent=`${tshirt.design[i].tsColor.split('.')[0]}`;
-  trEl.appendChild(tdEl3);
-  tbodyEl.appendChild(trEl);
-}
+
+  for (let i = 0; i < tshirt.design.length; i++) {
+    trEl=document.createElement('tr');
+    tdEl = document.createElement('td');
+    aEl =document.createElement('a');
+    aEl.setAttribute('id',`${i}`);
+    aEl.innerHTML='<i class="far fa-trash-alt"></i>';
+    aEl.addEventListener('click', removeItemFromCart);
+    tdEl.appendChild(aEl);
+    trEl.appendChild(tdEl);
+    let tdEl1 = document.createElement('td');
+    tdEl1.textContent=`${tshirt.design[i].quantity}`;
+    trEl.appendChild(tdEl1);
+    let tdEl2 = document.createElement('td');
+    tdEl2.textContent=`${tshirt.design[i].icon.split('.')[0]}`;
+    trEl.appendChild(tdEl2);
+    let tdEl3 = document.createElement('td');
+    tdEl3.textContent=`${tshirt.design[i].tsColor.split('.')[0]}`;
+    trEl.appendChild(tdEl3);
+    tbodyEl.appendChild(trEl);
+  }
 
 }
 
 
 function removeItemFromCart(event) {
-  
+
   console.log(event.target.id);
   event.preventDefault();
-removeItem(event.target.id) ;
-saveToLocalStorage();
-renderCart();
+  removeItem(event.target.id) ;
+  saveToLocalStorage();
+  renderCart();
 }
 
 
 function removeItem(index) {
 
-    tshirt.design.splice(index,1);
-    console.log(tshirt);
-  };
+  tshirt.design.splice(index,1);
+  console.log(tshirt);
+}
 
-  
+
 function saveToLocalStorage() {
 
-    let tShirtInCart = JSON.stringify(tshirt.design);
-    localStorage.setItem('t-shirtInCart',tShirtInCart);
-  };
-  
+  let tShirtInCart = JSON.stringify(tshirt.design);
+  localStorage.setItem('t-shirtInCart',tShirtInCart);
+}
+
 renderCart();
 // ******************************************************************************************
 // mone
 let openEl=document.getElementById('openBtn');
 
-openEl.addEventListener('click',showPop)
+openEl.addEventListener('click',showPop);
 
 function showPop(event){
 
-  document.querySelector(".popUpForm").style.display="flex"
+  document.querySelector('.popUpForm').style.display='flex';
   event.preventDefault();
 
 }
-// let closeForm=document.getElementById('closeBtn1')
-//   closeForm.addEventListener("click",unShow)
-  
-//   function unShow(event){
-//     event.preventDefault();
-//     closeForm.querySelector(".popUpForm").style.display="none"
-//   }
+let closeForm=document.getElementById('closeBtn1');
+closeForm.addEventListener('click',unShow);
+
+function unShow(event){
+
+  closeForm.querySelector('.popUpForm').style.display='none';
+  event.preventDefault();
+}
 
 
 // *****************************************************************************************************
