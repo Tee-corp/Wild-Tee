e6db7a45344304971c633bb10213df26c82efbe7
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-07-11 12:07:49 +0300

    added js

---
+++ JS/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let imgContainer = document.getElementById("ob");
+let imgFile = ["ballon.png", "bekind.png", "pizza.png", "ltcu.jpg", 'grunge.png', 'Etcher.png','logo.jpg','logo2.jpeg','a123 .png'];
+let imgEl;
+function render() {
+  for (let i = 0; i < imgFile.length; i++) {
+    let imgEl = document.createElement("img");
+    imgEl.setAttribute("src", "images/" + imgFile[i]);
+    imgEl.setAttribute("class", "item");
+    imgEl.setAttribute("width", "100px");
+    imgEl.setAttribute("id", i);
+
+    imgContainer.appendChild(imgEl);
+  }
+}
+render();
+
+
+imgContainer.addEventListener("click", goToCstmPage);
+
+let clickedicon='grunge.png';
+localStorage.setItem("clickedicon", clickedicon);
+
+function goToCstmPage(event) {
+  console.log(event.target.id);
+  let imgIndex = event.target.id
+   clickedicon= JSON.stringify(imgFile[imgIndex]);
+  localStorage.setItem("clickedicon", clickedicon);
+
+  location.href = "shirt.html";
+  
+}
+
+let btnEl= document.getElementById('btn');
+btnEl.addEventListener("click",goto);
+function goto (){
+  let imgIndex =2;
+  clickedicon= JSON.stringify(imgFile[imgIndex]);
+ localStorage.setItem("clickedicon", clickedicon);
+location.href = "shirt.html";
+
+}
+
+// ********************************************************************************************************************
+
+
+
---
+++ JS/cart.js
@@ -0,0 +1,83 @@
+'use strict';
+
+let  table = document.getElementById('cart');
+let tbodyEl = document.getElementById('body');
+table.appendChild(tbodyEl);
+
+let tshirt = {
+    design : [],
+}
+
+function loadCart() {
+  let  tShirtItems = JSON.parse(localStorage.getItem('t-shirtInCart')) || [];
+  tshirt.design= tShirtItems;
+  console.log(tshirt);
+}
+
+
+function renderCart() {
+  loadCart();
+  clearCart();
+  showCart();
+}
+
+function clearCart() {
+
+tbodyEl.textContent='';
+}
+
+
+let trEl;
+  let tdEl;
+  let aEl;
+function showCart() {
+  
+for (let i = 0; i < tshirt.design.length; i++) {
+   trEl=document.createElement('tr');
+  tdEl = document.createElement('td');
+   aEl =document.createElement('a');
+  aEl.setAttribute('id',`${i}`)
+  aEl.textContent='x';
+  aEl.addEventListener('click', removeItemFromCart);
+  tdEl.appendChild(aEl);
+  trEl.appendChild(tdEl);
+  let tdEl1 = document.createElement('td');
+  tdEl1.textContent=`${tshirt.design[i].quantity}`;
+  trEl.appendChild(tdEl1);
+  let tdEl2 = document.createElement('td');
+  tdEl2.textContent=`${tshirt.design[i].icon.split('.')[0]}`;
+  trEl.appendChild(tdEl2);
+  let tdEl3 = document.createElement('td');
+  tdEl3.textContent=`${tshirt.design[i].tsColor.split('.')[0]}`;
+  trEl.appendChild(tdEl3);
+  tbodyEl.appendChild(trEl);
+}
+
+}
+
+
+function removeItemFromCart(event) {
+  
+  console.log(event.target.id);
+  event.preventDefault();
+removeItem(event.target.id) ;
+saveToLocalStorage();
+renderCart();
+}
+
+
+function removeItem(index) {
+
+    tshirt.design.splice(index,1);
+    console.log(tshirt);
+  };
+
+  
+function saveToLocalStorage() {
+
+    let tShirtInCart = JSON.stringify(tshirt.design);
+    localStorage.setItem('t-shirtInCart',tShirtInCart);
+  };
+  
+renderCart();
+
