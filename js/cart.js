b9a642fea9f7c4ec2e00b6245971ab6bcec1a5d2
monaSalih <monaalobisat@gmail.com>
2021-07-12 14:57:37 +0300

    fexed

--- js/cart.js
+++ js/cart.js
@@ -35,15 +35,8 @@ function showCart() {
   for (let i = 0; i < tshirt.design.length; i++) {
     trEl=document.createElement('tr');
     tdEl = document.createElement('td');
-<<<<<<< HEAD
-    aEl =document.createElement('a');
-    aEl.setAttribute('id',`${i}`);
-    aEl.innerHTML='<i class="far fa-trash-alt"></i>';
-    aEl.addEventListener('click', removeItemFromCart);
-=======
     aEl = document.createElement('a');
     aEl.innerHTML = `<i id='${i}' class="far fa-trash-alt"></i>`;
->>>>>>> origin/main
     tdEl.appendChild(aEl);
     trEl.appendChild(tdEl);
     let tdEl1 = document.createElement('td');
@@ -52,8 +45,6 @@ function showCart() {
     let tdEl2 = document.createElement('td');
     tdEl2.textContent=`${tshirt.design[i].icon.split('.')[0]}`;
     trEl.appendChild(tdEl2);
-<<<<<<< HEAD
-=======
     // **************************************************************
     let tdEl4 = document.createElement('td');
     let img = document.createElement('img');
@@ -63,7 +54,6 @@ function showCart() {
     tdEl4.appendChild(img);
 // *******************************************************************
     trEl.appendChild(tdEl4);
->>>>>>> origin/main
     let tdEl3 = document.createElement('td');
     tdEl3.textContent=`${tshirt.design[i].tsColor.split('.')[0]}`;
     trEl.appendChild(tdEl3);
@@ -73,12 +63,7 @@ function showCart() {
 
 
 function removeItemFromCart(event) {
-<<<<<<< HEAD
-
-  console.log(event.target.id);
-=======
  
->>>>>>> origin/main
   event.preventDefault();
   removeItem(event.target.id) ;
   saveToLocalStorage();
@@ -86,15 +71,9 @@ function removeItemFromCart(event) {
 }
 
 
-<<<<<<< HEAD
-  tshirt.design.splice(index,1);
-  console.log(tshirt);
-}
-=======
 function removeItem(index) {
   tshirt.design.splice(Number(index),1); 
 };
->>>>>>> origin/main
 
 
 function saveToLocalStorage() {
